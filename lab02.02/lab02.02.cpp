a2baa450cf900a46298fc141b8c4fd5d7697547e
KBigus04 <kinga.bigus04@o2.pl>
2025-10-28 00:27:42 +0100

    lab02.02

--- lab02.02/lab02.02.cpp
+++ lab02.02/lab02.02.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstdlib>
 using namespace std;
 
 //zad11
@@ -28,15 +29,78 @@ using namespace std;
 }*/
 
 //zad12
-int main() {
+/*int main() {
 	int n;
-	float l, suma = 0;
+	float l, suma=0;
 	cout << "Podaj ilosc liczb do sredniej arytmetycznej: ";
 	cin >> n;
-	for (int i = 0; i < n; i++) {
-		cout << "\nPodaj liczbe: ";
-		cin >> l;
-		suma = l + l;
+	if (n > 0) {
+		for (int i = 0; i < n; i++) {
+			cout << "\nPodaj liczbe: ";
+			cin >> l;
+			suma = suma + l;
+		}
+		cout << "\nSrednia arytmetyczna n liczb wprowadzonych z klawiatury to: " << suma / n;
 	}
-	cout << "\nSrednia arytmetyczna n liczb wprowadzonych z klawiatury to: " << suma / n;
-}
+	else {
+		cout << "Liczba n musi byc dodatnia";
+	}
+}*/
+
+//zad 13
+/*int main() {
+	for (int i = 100; i >= 0; i--) {
+		cout << i << endl;
+	}
+}*/
+
+//zad14
+/*int main() {
+	int a, b, c;
+	cout << "Podaj 3 cyfry: \n";
+	cin >> a;
+	cin >> b;
+	cin >> c;
+	if (a < b && a < c) {
+		cout << "Najmniejsza z liczb to: " << a;
+	}
+	else if (b < c) {
+		cout << "Najmniejsza z liczb to: " << b;
+	}
+	else {
+		cout << "Najmniejsza z liczb to: " << c;
+	}
+}*/
+
+//zad15
+/*int main() {
+	int n, m, r;
+	cout << "Podaj ilosc losowanych liczb (n): ";
+	cin >> n;
+	cout << "\nPodaj koncowa wartosc przedzialu (m): ";
+	cin >> m;
+	for (int i = 0; i < n; i++) {
+		r = rand() % m + 1;
+		cout << r << endl;
+	}
+}*/
+
+//zad16
+/*int main() {
+	int n,r,k=0;
+	cout << "Podaj szczesliwy numerek z przedzialu [1,10]: ";
+	cin >> n;
+	if (n < 1 || n>10) {
+		cout << "\nNumerek nie zawiera sie w przedziale!";
+	}
+	else {
+		for (int i = 0; i < 10; i++) {
+			r = rand() % 11;
+			cout << r << ", ";
+			if (r == n) {
+				k++;
+			}
+		}
+		cout << "\n\nTwoj szczesliwy numerek pojawil sie " << k << " razy posrod wylosowanych liczb\n";
+	}
+}*/
