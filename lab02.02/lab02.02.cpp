bc60c74d4b815049910f9eb3011711692275e33d
KBigus04 <kinga.bigus04@o2.pl>
2025-10-27 23:31:13 +0100

    lab0202

--- lab02.02/lab02.02.cpp
+++ lab02.02/lab02.02.cpp
@@ -1,8 +1,42 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-	<< "Hello World!\n";
-}
+//zad11
+/*int main() {
+	int a,b;
+	cout << "Podaj wartosc a: ";
+	cin >> a;
+	cout << "\nPodaj wartosc b: ";
+	cin >> b;
+	if (a == b) {
+		cout << "\nWartosci a i b musza byc od siebie ro¿ne!";
+	}
+	else if (a < b) {
+		for (int i = a; i <= b; i++) {
+			if (i % 3 == 0) {
+				cout << i << endl;
+			}
+		}
+	}
+	else {
+		for (int i = b; i <= a; i++) {
+			if (i % 3 == 0) {
+				cout << i << endl;
+			}
+		}
+	}
+}*/
 
+//zad12
+int main() {
+	int n;
+	float l, suma = 0;
+	cout << "Podaj ilosc liczb do sredniej arytmetycznej: ";
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cout << "\nPodaj liczbe: ";
+		cin >> l;
+		suma = l + l;
+	}
+	cout << "\nSrednia arytmetyczna n liczb wprowadzonych z klawiatury to: " << suma / n;
+}
